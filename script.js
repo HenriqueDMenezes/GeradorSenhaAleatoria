67af1bbcb9ea26e7afef046b68607bfd8719584b
HenriqueDMenezes <henriquebm16@hotmail.com>
2021-09-13 18:28:48 -0300

    adicionada função de copiar

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var alertBox = document.querySelector('.alertBox');
+
+
 function getPassword(){
   var chars ="0123456789asdfghjklçmnbvcxzqwertyuiop!@#$%&*()<>{}[]"
   var passwordLength = 16;
@@ -9,4 +12,13 @@ function getPassword(){
   }
   document.getElementById("password").value = password;
 }
+
+function copia(){
+  var copiaDaSenha = document.getElementById("password");
+  copiaDaSenha.Select();
+  copiaDaSenha.setSelectionRange(0,9999);
+  document.execCommand('copia');
+  alertBox.classList.toggle('active');
+}
+
 
